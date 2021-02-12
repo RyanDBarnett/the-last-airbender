f9cc52c73f5a3715c292471f0349bd27c7992d60
RyanDBarnett <conner2468@gmail.com>
2021-02-12 10:52:12 -0700

    Add member name test

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,4 +1,32 @@
 class SearchController < ApplicationController
   def index
+    page_number = 1
+    results = []
+    page = []
+
+    while page_number == 1 || page.size == 25 do
+      page = get_page(page_number)
+      results = results.concat(page)
+      page_number += 1
+    end
+
+    @total_search_results = results.size
+    @members = results.first(25)
+  end
+
+  private
+
+  def get_page page_number
+    response = conn.get("/api/v1/characters?affiliation=#{search_params[:nation]}&perPage=25&page=#{page_number}")
+    JSON.parse(response.body, symbolize_names: true)
+  end
+
+  def conn
+    Faraday.new('http://last-airbender-api.herokuapp.com')
+  end
+
+  def search_params
+    params[:nation] = params[:nation].split('_').map{ |element| element.capitalize}.join('+')
+    params.permit(:nation)
   end
 end
---
+++ spec/features/search/index_spec.rb
@@ -0,0 +1,39 @@
+require 'rails_helper'
+
+RSpec.describe 'As a user' do
+  describe "When I am taken to '/search' after performing a 'Fire Nation' search" do
+    it "Then I should see the total number of people who live in the Fire Nation." do
+      visit root_path
+
+      select 'Fire Nation', from: :nation
+
+      click_button 'Search For Members'
+
+      expect(page).to have_content("Total Members in Fire Nation: 97")
+    end
+
+    describe 'I should see a list of members of the Fire Nation.' do
+      it 'the list should only be 25 members' do
+        visit root_path
+
+        select 'Fire Nation', from: :nation
+
+        click_button 'Search For Members'
+
+        expect(page).to have_css('.member', count: 25)
+      end
+
+      describe 'For each of the members I should see:' do
+        it 'the name of the member' do
+          visit root_path
+
+          select 'Fire Nation', from: :nation
+
+          click_button 'Search For Members'
+
+          expect(find(".member", match: :first)).to have_content("Name: Afiko")
+        end
+      end
+    end
+  end
+end
